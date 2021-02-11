0ce9474ec4d55210a63d9e473a827b6b364fbfaa
haki9975 <elias.robert.hakim@gmail.com>
2021-02-11 17:58:29 -0500

    commit tree 8b5706b0d14cef10f3f2281626126792c2a1cbd6

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :student, :grade, :roster, :students
+
+
+def initialize(school)
+    @roster = {}
+    @students = []
+end
+def add_student(student, number)
+    @students << student
+    @roster.key?(number) ? @roster[number] << student : @roster[number] = [student]
+end
+def grade(grade)
+    @roster[grade]
+end
+def sort
+    sorted = {}
+    @roster.each {|grades, students| sorted[grades] = students.sort }
+ return sorted
+end
+
+end
